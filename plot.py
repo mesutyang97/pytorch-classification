5e0ac0a5fbed2890d735e005276b03ef0597d8f4
Xiaocheng Yang (Mesut) <mesut.yang97@gmail.com>
2018-06-16 01:17:23 -0700

    updating plot function

--- plot.py
+++ plot.py
@@ -10,14 +10,14 @@ import matplotlib.pyplot as plt
 NN = "vgg19_bn"
 DATA = "cifar10"
 PATH_str = "checkpoints/cifar10/vgg19_bn/log-temp{}.txt"
-TEMPS = [0.5, 1.0]
+TEMPS = [0.125, 0.25, 0.5, 1.0, 4.0, 16.0]
 TRAINSIZE = 3
 
 COLORS = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
 
 
 def main():
-	plt.figure()
+	plt.figure(figsize=(18, 8))
 	plt.title('{}-{} with different temps'.format(
         DATA, NN))
 	plt.xlabel("Epochs")
@@ -29,10 +29,10 @@ def main():
 			train_error = np.subtract(100, Data[3])
 			vali_error = np.subtract(100, Data[4])
 
-			plt.plot(range(TRAINSIZE), train_error, '-', color=COLORS[i],
+			plt.plot(range(TRAINSIZE), train_error, ':', color=COLORS[i],
 	             label="Training error with temp {}".format(temp))
 
-			plt.plot(range(TRAINSIZE), vali_error, ':', color=COLORS[i],
+			plt.plot(range(TRAINSIZE), vali_error, '-', color=COLORS[i],
 	             label="vali_error error with temp {}".format(temp))
 
 			print(train_error)

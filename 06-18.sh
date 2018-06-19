f0057a3714e69f8fc9d263f975aee3263796230d
Xiaocheng Yang (Mesut) <mesut.yang97@gmail.com>
2018-06-18 22:25:42 -0700

    finished structure upgrade

---
+++ 06-18.sh
@@ -0,0 +1,6 @@
+python plot.py -a resnet-110 -d cifar10
+python plot.py -a resnet-110 -d cifar100
+python plot.py -a densenet-bc-100-12 -d cifar10
+python plot.py -a densenet-bc-100-12 -d cifar100
+python plot.py -a vgg19_bn -d cifar10
+python plot.py -a vgg19_bn -d cifar100
--- plot.py
+++ plot.py
@@ -5,43 +5,64 @@ Copyright Xiaocheng Yang, 2018
 
 
 import numpy as np
+import argparse
+import time
 import matplotlib.pyplot as plt
 
-NN = "vgg19_bn"
-DATA = "cifar10"
-PATH_str = "checkpoints/cifar10/vgg19_bn/log-temp{}.txt"
-TEMPS = [0.125, 0.25, 0.5, 1.0, 4.0, 16.0]
-TRAINSIZE = 3
+# Base_PATH = "../code/ts/cifar/checkpoints/"
+Base_PATH = "checkpoints/{}/{}_{}/log.txt"
+Graph_PATH = "graph"
+TEMPS = [0.1, 0.3, 1, 3, 10, 30]
 
 COLORS = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
 
 
-def main():
-	plt.figure(figsize=(18, 8))
-	plt.title('{}-{} with different temps'.format(
-        DATA, NN))
-	plt.xlabel("Epochs")
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10/100 Result Plotting')
+# Datasets
+parser.add_argument('-d', '--dataset', default='cifar10', type=str)
+
+# Architecture
+parser.add_argument('--arch', '-a', default='vgg19_bn', type=str)
+
+args = parser.parse_args()
+
+def errorCurve(dataset, arch, trainsize):
+	plt.figure(figsize=(8, 8))
+	plt.title('{} {} final error with different temps'.format(
+        dataset, arch))
+	plt.xlabel("log(temps)")
 	plt.ylabel("Error (%)")
+	train_error_temps = []
+	vali_error_temps = []
+
+	logtemp = np.log(TEMPS)
+
 	for i, temp in enumerate(TEMPS):
-		with open(PATH_str.format(temp)) as f:
+		with open(Base_PATH.format(dataset, arch, temp)) as f:
 			lines = (line.rstrip() for line in f)
 			Data = np.loadtxt(lines, delimiter='\t', skiprows=1).transpose()
-			train_error = np.subtract(100, Data[3])
-			vali_error = np.subtract(100, Data[4])
+			train_error_temps.append(100 - Data[3][-1])
+			vali_error_temps.append(100 - Data[4][-1])
 
-			plt.plot(range(TRAINSIZE), train_error, ':', color=COLORS[i],
-	             label="Training error with temp {}".format(temp))
+	
+	plt.plot(logtemp, train_error_temps, ':', color=COLORS[i],
+	        label="Training error")
 
-			plt.plot(range(TRAINSIZE), vali_error, '-', color=COLORS[i],
-	             label="vali_error error with temp {}".format(temp))
+	plt.plot(logtemp, vali_error_temps, '-', color=COLORS[i],
+	        label="vali_error error")
 
-			print(train_error)
-
-	filename = '{}-{}.png'.format(
-        DATA, NN)
+	axes = plt.gca()
+	axes.set_ylim([0,30])
+	filename = 'final_error-{}-{}-{}.png'.format(
+        dataset,arch, time.time())
 	plt.legend(loc="best")
 	plt.savefig(filename)
-
+def main():
+	# trainingCurve()
+	trainsize = 164
+	if args.arch == 'densenet-bc-100-12':
+		trainsize = 300
+	errorCurve(args.dataset, args.arch, trainsize)
 
 if __name__ == '__main__':
     main()
